28a7cf8a03b034de86a2ba0a63f61639137e98cf
Alex <44127001+deadlynjdj@users.noreply.github.com>
2024-01-18 19:49:27 -0500

    Refactor form data handling in index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,16 @@ const router = Router();
 router.post('/:key/:event',  async (request) => {
 	const { params } = request
 	const formData = await request.formData()
-	const json = formData.get('payload')
+	const output: { [key: string]: FormDataEntryValue | FormDataEntryValue[]; payload?: any } = {}
+	for (const [key, value] of formData) {
+		const tmp = output[key]
+		if (tmp === undefined) {
+			output[key] = value
+		} else {
+			output[key] = [].concat(tmp, [value])
+		}
+	}
+	let json = (JSON.stringify(output.payload, null, 2)).replace(/\\/g, '')
 	const response = await fetch(`https://maker.ifttt.com/trigger/${params.event}/json/with/key/${params.key}`, {
 		method: 'POST',
 		body: json

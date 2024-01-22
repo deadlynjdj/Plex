14354c1229618d4fa4dd5431c68a0086b9cae1d8
Alex <44127001+deadlynjdj@users.noreply.github.com>
2024-01-21 19:06:22 -0500

    Refactor form data handling and add JSON content type header

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,9 @@ const router = Router();
 router.post('/:key/:event',  async (request) => {
 	const { params } = request
 	const formData = await request.formData()
-	const output: { [key: string]: FormDataEntryValue | FormDataEntryValue[]; payload?: any } = {}
-	for (const [key, value] of formData) {
-		const tmp = output[key]
-		if (tmp === undefined) {
-			output[key] = value
-		} else {
-			output[key] = [].concat(tmp, [value])
-		}
-	}
-	let json = (JSON.stringify(output.payload, null, 2)).replace(/\\/g, '')
+	const json = formData.get('payload')
 	const response = await fetch(`https://maker.ifttt.com/trigger/${params.event}/json/with/key/${params.key}`, {
+		headers: { 'Content-Type': 'application/json' },
 		method: 'POST',
 		body: json
 	})
@@ -24,9 +16,7 @@ router.post('/:key/:event',  async (request) => {
 		status: response.status
 	})
 });
-
 router.all('*', () => Response.redirect('https://github.com/deadlynjdj/plex', 302));
-
 export default {
 	fetch: router.handle,
 };
